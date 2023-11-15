cc46b2a642e699f2e3e6a956e4163268d4ceaa40
nathCota <nathy220695@hotmail.com>
2023-11-15 18:57:26 -0300

    Algoritmo Guloso

---
+++ src/main/java/app.java
@@ -0,0 +1,7 @@
+public class app {
+    public static void main(String[] args) {
+        int[] tamanhosConjunto = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+     EstrategiaGulosa.testarEstrategias(tamanhosConjunto);
+    }
+
+}
